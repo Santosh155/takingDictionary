f1e84cd068949108ac38ddfb9b07b3d90ae0a615
santosh <khadkasantosh009@gmail.com>
2021-05-30 23:57:50 +0545

    footer component added

---
+++ frontend/taking-dictionary/src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+            &copy; {new Date().getFullYear()} Santosh Khadka
+        </div>
+    );
+};
+
+export default Footer;
