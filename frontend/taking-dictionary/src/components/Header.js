4626c57e1ecae2f0514f1cf7861aebe59fabad9d
santosh <khadkasantosh009@gmail.com>
2021-05-30 23:15:23 +0545

    header component added

---
+++ frontend/taking-dictionary/src/components/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <div>
+                <img src="" alt="logo" />
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+            </div>
+            <div>
+                <a href="#">Login</a>
+                <a href="#">Signup</a>
+                <a href="#">Saved Words</a>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
