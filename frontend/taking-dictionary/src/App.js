428da768f6359a3d6717b2e47405e22848a17aa1
santosh <khadkasantosh009@gmail.com>
2021-05-31 00:10:57 +0545

    simple style added

--- frontend/taking-dictionary/src/App.js
+++ frontend/taking-dictionary/src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Dictionary from './components/Dictionary';
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <Dictionary />
             <Footer />
         </div>
     );
---
+++ frontend/taking-dictionary/src/components/Dictionary.js
@@ -0,0 +1,17 @@
+const Dictionary = () => {
+    return (
+        <div style={{ textAlign: 'center', margin: '50px' }}>
+            <h2>Search for a word</h2>
+            <form method="get">
+                <input
+                    type="text"
+                    placeholder="Enter words"
+                    className="dictionary"
+                />
+                <input type="button" />
+            </form>
+        </div>
+    );
+};
+
+export default Dictionary;
