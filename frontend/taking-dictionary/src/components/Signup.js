ab07ef5bdc7ae3a1bc8fc75962bba8c41368ea5c
santosh <khadkasantosh009@gmail.com>
2021-05-31 16:47:02 +0545

    login and signup styled

--- frontend/taking-dictionary/src/components/Signup.js
+++ frontend/taking-dictionary/src/components/Signup.js
@@ -1,13 +1,54 @@
 // import 'bootstrap/dist/css/bootstrap.min.css';
 const Signup = () => {
     return (
-        <div className="container">
+        <div className="container" style={{ width: '40%' }}>
             <form method="post">
-                Full Name: <input type="text" name="name" /> <br />
-                Email: <input type="email" name="email" /> <br />
-                Password: <input type="password" name="password" /> <br />
-                Address: <input type="address" name="address" /> <br />
-                <input type="submit" />
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">
+                        Full Name
+                    </label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        id="exampleInputEmail1"
+                        aria-describedby="emailHelp"
+                    />
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">
+                        Current Address
+                    </label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        id="exampleInputEmail1"
+                        aria-describedby="emailHelp"
+                    />
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">
+                        Email address
+                    </label>
+                    <input
+                        type="email"
+                        class="form-control"
+                        id="exampleInputEmail1"
+                        aria-describedby="emailHelp"
+                    />
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">
+                        Password
+                    </label>
+                    <input
+                        type="password"
+                        class="form-control"
+                        id="exampleInputPassword1"
+                    />
+                </div>
+                <button type="submit" class="btn btn-primary">
+                    Submit
+                </button>
             </form>
         </div>
     );
