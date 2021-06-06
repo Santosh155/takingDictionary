4059d39011e986380e93041359076690dc5d4ed4
santosh <khadkasantosh009@gmail.com>
2021-06-06 16:35:01 +0545

    simple error fixes

--- frontend/taking-dictionary/src/components/Contact.js
+++ frontend/taking-dictionary/src/components/Contact.js
@@ -2,37 +2,30 @@ const Contact = () => {
     return (
         <div className="container" style={{ marginTop: '40px', width: '50%' }}>
             <form method="get">
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">
-                        Name
-                    </label>
+                <div className="mb-3">
+                    <label className="form-label">Name</label>
                     <input
                         type="text"
-                        class="form-control"
-                        id="exampleInputEmail1"
+                        className="form-control"
                         aria-describedby="emailHelp"
                     />
                 </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">
-                        email
-                    </label>
+                <div className="mb-3">
+                    <label className="form-label">email</label>
                     <input
                         type="email"
-                        class="form-control"
-                        id="exampleInputEmail1"
+                        className="form-control"
                         aria-describedby="emailHelp"
                     />
                 </div>
-                <div class="mb-3">
-                    <label for="floatingTextarea">Message</label>
+                <div className="mb-3">
+                    <label>Message</label>
                     <textarea
-                        class="form-control"
+                        className="form-control"
                         placeholder="Leave a message here"
-                        id="floatingTextarea"
                     ></textarea>
                 </div>
-                <button type="submit" class="btn btn-primary">
+                <button type="submit" className="btn btn-primary">
                     Submit
                 </button>
             </form>
