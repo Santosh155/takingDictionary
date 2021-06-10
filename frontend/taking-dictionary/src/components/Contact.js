93a426b11b9c1d8c37ed4b55d0d71fa10d67beff
santosh <khadkasantosh009@gmail.com>
2021-06-10 23:28:14 +0545

    user can send feedback through contact page

--- frontend/taking-dictionary/src/components/Contact.js
+++ frontend/taking-dictionary/src/components/Contact.js
@@ -1,13 +1,43 @@
+import { useState } from 'react';
+import Axios from 'axios';
+
 const Contact = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [showMessage, setShowMessage] = useState('');
+    const contact = (e) => {
+        e.preventDefault();
+        Axios.post('http://localhost:5000/api/v1/contact', {
+            name: name,
+            email: email,
+            message: message,
+        })
+            .then((response) => {
+                setShowMessage(response.data.message);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
     return (
         <div className="container" style={{ marginTop: '40px', width: '50%' }}>
-            <form method="get">
+            {showMessage ? (
+                <p className="alert alert-primary" role="alert">
+                    {showMessage}
+                </p>
+            ) : null}
+            <form method="post" onSubmit={contact}>
                 <div className="mb-3">
                     <label className="form-label">Name</label>
                     <input
                         type="text"
                         className="form-control"
                         aria-describedby="emailHelp"
+                        required
+                        onChange={(e) => {
+                            setName(e.target.value);
+                        }}
                     />
                 </div>
                 <div className="mb-3">
@@ -16,6 +46,10 @@ const Contact = () => {
                         type="email"
                         className="form-control"
                         aria-describedby="emailHelp"
+                        required
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
                     />
                 </div>
                 <div className="mb-3">
@@ -23,6 +57,10 @@ const Contact = () => {
                     <textarea
                         className="form-control"
                         placeholder="Leave a message here"
+                        required
+                        onChange={(e) => {
+                            setMessage(e.target.value);
+                        }}
                     ></textarea>
                 </div>
                 <button type="submit" className="btn btn-primary">
