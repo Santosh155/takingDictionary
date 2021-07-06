ca545d81e87332135a6f0b4e727a8e0c1e3b333d
santosh <khadkasantosh009@gmail.com>
2021-07-06 17:10:40 +0545

    validation is set for signup form

--- frontend/taking-dictionary/src/components/Signup.js
+++ frontend/taking-dictionary/src/components/Signup.js
@@ -1,35 +1,46 @@
 import { useState } from 'react';
 import Axios from 'axios';
+import Validation from './Validation';
 
 const Signup = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [address, setAddress] = useState('');
-    const [error, setError] = useState('');
-
+    const [errors, setErrors] = useState({});
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        password: '',
+        address: '',
+    });
+    const handleChoice = (event) => {
+        setValues({
+            ...values,
+            [event.target.name]: event.target.value,
+        });
+    };
     const Register = (e) => {
         e.preventDefault();
+        setErrors(Validation(values));
         Axios.post('http://localhost:5000/api/v1/register', {
-            name: name,
-            email: email,
-            password: password,
-            address: address,
+            name: values.name,
+            email: values.email,
+            password: values.password,
+            address: values.address,
         })
             .then((response) => {
-                console.log(response);
+                console.log(response.json());
+                window.location.assign('/login');
             })
             .catch((err) => {
-                setError(err.response.data.message);
+                // setErrors(err.response.data.message);
+                console.log(err.response.data.message);
             });
     };
     return (
         <div className="container" style={{ width: '40%', marginTop: '40px' }}>
-            {error ? (
+            {/* {error ? (
                 <p className="alert alert-danger" role="alert">
                     {error}
                 </p>
-            ) : null}
+            ) : null} */}
             <form onSubmit={Register}>
                 <div className="mb-3">
                     <label className="form-label">Full Name</label>
@@ -37,48 +48,48 @@ const Signup = () => {
                         type="text"
                         className="form-control"
                         aria-describedby="emailHelp"
-                        onChange={(e) => {
-                            setName(e.target.value);
-                        }}
-                        required
+                        onChange={handleChoice}
+                        name="name"
                     />
                 </div>
+                {errors.name && <p style={{ color: 'red' }}>{errors.name}</p>}
                 <div className="mb-3">
                     <label className="form-label">Current Address</label>
                     <input
                         type="text"
                         className="form-control"
                         aria-describedby="emailHelp"
-                        onChange={(e) => {
-                            setAddress(e.target.value);
-                        }}
-                        required
+                        onChange={handleChoice}
+                        name="address"
                     />
                 </div>
+                {errors.address && (
+                    <p style={{ color: 'red' }}>{errors.address}</p>
+                )}
                 <div className="mb-3">
                     <label className="form-label">Email address</label>
                     <input
                         type="email"
                         className="form-control"
                         aria-describedby="emailHelp"
-                        onChange={(e) => {
-                            setEmail(e.target.value);
-                        }}
-                        required
+                        onChange={handleChoice}
+                        name="email"
                     />
                 </div>
+                {errors.email && <p style={{ color: 'red' }}>{errors.email}</p>}
                 <div className="mb-3">
                     <label className="form-label">Password</label>
                     <input
                         type="password"
                         className="form-control"
                         id="exampleInputPassword1"
-                        onChange={(e) => {
-                            setPassword(e.target.value);
-                        }}
-                        required
+                        onChange={handleChoice}
+                        name="password"
                     />
                 </div>
+                {errors.password && (
+                    <p style={{ color: 'red' }}>{errors.password}</p>
+                )}
                 <button type="submit" className="btn btn-primary">
                     Submit
                 </button>
